511393ff5aeef704fe031d33e128be381dbe315e
cybermanii <programdor.negro@gmail.com>
2023-07-21 19:51:09 -0500

    initial commit

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,16 @@
+<template lang="es">
+    <div>
+        <h1>{{ msg }}</h1>
+        <p>This is the login page</p>
+
+        
+    </div>
+</template>
+<script>
+export default {
+  name: 'LoginComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template lang="es">
+    <div>
+        <LoginComponent msg="Welcome to the Login page"/>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Login',
+  components: {
+    LoginComponent
+  },
+  created() {
+      
+  },
+}
+</script>
